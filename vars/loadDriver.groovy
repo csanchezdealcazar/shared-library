089339086b89f98093e9b564e13c999afe42513f
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 11:05:46 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -1,21 +1,10 @@
 
-@Grab('mysql:mysql-connector-java:5.1.10')
+	@Grab('com.oracle:ojdbc6:11.2.0.4')
+	import groovy.sql.Sql;
+    import java.util.ServiceLoader;
+    import java.sql.Driver;
 
-
-import groovy.sql.Sql;
-import java.util.ServiceLoader;
-import java.sql.*;
-
-def call(Map config){
-
- Class.forName("com.mysql.jdbc.Driver");  
-Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo","root","root");  
- 
-Statement stmt=con.createStatement();  
-ResultSet rs=stmt.executeQuery("select * from emp");  
-while(rs.next())  
-System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-con.close();  
-
-  
-}
+    ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
+    def sql = Sql.newInstance('jdbc:oracle:thin:@SERVER:2483/INSTANCE', 'USER', 'PASSWORD', 'oracle.jdbc.OracleDriver')
+    sql.execute 'select 1 from dual'
+    sql.close()
