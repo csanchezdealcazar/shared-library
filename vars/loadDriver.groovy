657d29503287566afb6d72b2dc32759155b6d522
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 11:01:36 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -8,8 +8,16 @@ import java.sql.Connection;
 import java.sql.Driver;
 
 def call(Map config){
-ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
-sql = Sql.newInstance('jdbc:mysql://localhost:3306/test', 'root', 'root', 'com.mysql.jdbc.Driver')
-sql.execute 'select 1 from dual'
-sql.close()
+
+ Class.forName("com.mysql.jdbc.Driver");  
+Connection con=DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/sonoo","root","root");  
+ 
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from emp");  
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+con.close();  
+}catch(Exception e){ System.out.println(e);}  
+}  
 }
