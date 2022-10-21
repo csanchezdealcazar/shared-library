40a372d40434bfbe946aaf7d87125ece186467cb
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 10:47:39 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -1,13 +1,14 @@
 
-@Grab('mysql:mysql-connector-java:5.1.25')
+@Grab("mysql:mysql-connector-java:5.1.25")
+@GrabConfig(systemClassLoader=true)
 
 import groovy.sql.Sql;
 import java.util.ServiceLoader;
 import java.sql.Driver;
 
 def call(Map config){
-Class.forName("com.mysql.jdbc.Driver")
-sql = Sql.newInstance("jdbc:mysql://172.17.0.2:3306/zd190955", "root","root", "com.mysql.jdbc.Driver")
-rows = sql.execute "select * from sample;"
-echo rows.dump()
+ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
+sql = Sql.newInstance('jdbc:mysql://localhost:3306/test', 'root', 'root', 'com.mysql.jdbc.Driver')
+sql.execute 'select 1 from dual'
+sql.close()
 }
