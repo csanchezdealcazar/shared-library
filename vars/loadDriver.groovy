87f922fccc7eda65bc358cef5c0c8e28466e37ad
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 11:31:34 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -1,9 +1,9 @@
-@Grab('mysql:mysql-connector-java:5.1.25')
 
 import groovy.sql.Sql;
 import java.util.ServiceLoader;
 import java.sql.Driver;
 
+@Grab('mysql:mysql-connector-java:5.1.25')
 def call(Map config){
 ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
 sql = Sql.newInstance('jdbc:mysql://localhost:3306/test', 'root', 'root', 'com.mysql.jdbc.Driver')
