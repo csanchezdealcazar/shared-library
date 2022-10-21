70407174d8309abeb32eb84dea9e69198d80aaf7
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 10:28:08 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -6,7 +6,7 @@ import java.util.ServiceLoader;
 import java.sql.Driver;
 
 def call(Map config){
-ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
+Class.forName("com.mysql.jdbc.Driver"); 
 sql = Sql.newInstance("jdbc:mysql://localhost:3306/test", "root", "root", "com.mysql.jdbc.Driver")
 sql.execute 'select 1 from dual'
 sql.close()
