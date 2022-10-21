65518788cc1377dffa253b8d636fc2e2fe7e423e
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 11:51:47 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -1,4 +1,5 @@
 
+import java.sql.DriverManager;
 import groovy.sql.Sql
 
 
@@ -13,11 +14,14 @@ class MyDB {
  /* def db = Sql.newInstance("jdbc:mysql://hostname/database", "username", "password", "com.mysql.jdbc.Driver")*/
 
  
-  Class.forName("com.mysql.jdbc.Driver")
+ 
+ // Class.forName("com.mysql.jdbc.Driver")
   def getUsers() {
-    def sql = Sql.newInstance("jdbc:mysql://mysql:3306/test_db", "user","passwd", "com.mysql.jdbc.Driver")
+ /*   def sql = Sql.newInstance("jdbc:mysql://mysql:3306/test_db", "user","passwd", "com.mysql.jdbc.Driver")
     def rows = sql.execute "select count(*) from test_table;"
-    echo rows.dump()
+    echo rows.dump()*/
+   def conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+                                   "user=minty&password=greatsqldb");
   }
   
  /* 
