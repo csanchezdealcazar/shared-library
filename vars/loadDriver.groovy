91e7c163b20214c710fcb89aaf88b45c3ac00c6a
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 10:22:34 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -5,7 +5,9 @@ import groovy.sql.Sql;
 import java.util.ServiceLoader;
 import java.sql.Driver;
 
+def call(Map config){
 ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
-def sql = Sql.newInstance('jdbc:oracle:thin:@SERVER:2483/INSTANCE', 'USER', 'PASSWORD', 'oracle.jdbc.OracleDriver')
+sql = Sql.newInstance('jdbc:oracle:thin:@SERVER:2483/INSTANCE', 'USER', 'PASSWORD', 'oracle.jdbc.OracleDriver')
 sql.execute 'select 1 from dual'
 sql.close()
+}
