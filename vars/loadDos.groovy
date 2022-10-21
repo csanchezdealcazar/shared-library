434a026fc6d4b64276a33006037276d43ebc8242
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 15:47:36 +0200

    Update loadDos.groovy

--- vars/loadDos.groovy
+++ vars/loadDos.groovy
@@ -5,9 +5,11 @@
 import groovy.sql.*
 import java.sql.DriverManager
 
-DriverManager.registerDriver(new com.mysql.jdbc.Driver())
+def call(Map Config){
+    DriverManager.registerDriver(new com.mysql.jdbc.Driver())
 
-def logger = LoggerFactory.getLogger('sql')
-logger.info 'Initialize SQL'
-def sql = Sql.newInstance('jdbc:mysql://localhost:3306/midb, 'USER', 'PASSWORD', 'com.mysql.jdbc.Driver')
-logger.info "Got myself a SQL connection: $sql"
+    def logger = LoggerFactory.getLogger('sql')
+    logger.info 'Initialize SQL'
+    def sql = Sql.newInstance('jdbc:mysql://localhost:3306/midb, 'USER', 'PASSWORD', 'com.mysql.jdbc.Driver')
+    logger.info "Got myself a SQL connection: $sql"
+}
