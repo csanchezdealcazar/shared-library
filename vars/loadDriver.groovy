26b645c7e50f8fdbfceaeb48f8a0ee3203e59fe7
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 11:06:51 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -1,10 +1,12 @@
+@Grab('mysql:mysql-connector-java:5.1.25')
 
-	@Grab('com.oracle:ojdbc6:11.2.0.4')
-	import groovy.sql.Sql;
-    import java.util.ServiceLoader;
-    import java.sql.Driver;
+import groovy.sql.Sql;
+import java.util.ServiceLoader;
+import java.sql.*;
 
-    ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
-    def sql = Sql.newInstance('jdbc:oracle:thin:@SERVER:2483/INSTANCE', 'USER', 'PASSWORD', 'oracle.jdbc.OracleDriver')
-    sql.execute 'select 1 from dual'
-    sql.close()
+def call(Map config){
+ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
+sql = Sql.newInstance('jdbc:mysql://localhost:3306/test', 'root', 'root', 'com.mysql.jdbc.Driver')
+sql.execute 'select 1 from dual'
+sql.close()
+}
