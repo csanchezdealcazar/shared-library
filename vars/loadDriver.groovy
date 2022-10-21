f0e8a228f3c01aa4affac4474c45be9c15ce24b3
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 10:39:21 +0200

    ejemplo oracle

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -1,13 +1,10 @@
 
-@Grab("mysql:mysql-connector-java:5.1.25")
+@Grab('com.oracle:ojdbc6:11.2.0.4')
+	import groovy.sql.Sql;
+    import java.util.ServiceLoader;
+    import java.sql.Driver;
 
-import groovy.sql.Sql;
-import java.util.ServiceLoader;
-import java.sql.Driver;
-
-def call(Map config){
-ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
-sql = Sql.newInstance('jdbc:mysql://localhost:3306/test', 'root', 'root', 'com.mysql.jdbc.Driver')
-sql.execute 'select 1 from dual'
-sql.close()
-}
+    ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
+    def sql = Sql.newInstance('jdbc:oracle:thin:@SERVER:2483/INSTANCE', 'USER', 'PASSWORD', 'oracle.jdbc.OracleDriver')
+    sql.execute 'select 1 from dual'
+    sql.close()
