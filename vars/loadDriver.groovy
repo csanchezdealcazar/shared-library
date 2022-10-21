24b197e7c1a49c0914eb8e368e97ff3db55a4567
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 12:16:10 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -1,7 +1,7 @@
 import java.sql.DriverManager;
 import groovy.sql.Sql
 
-@Grab(group='mysql', module='mysql-connector-java', version='8.0.21')
+//@Grab(group='mysql', module='mysql-connector-java', version='8.0.21')
 class MyDB {
     def driver = Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); 
     
