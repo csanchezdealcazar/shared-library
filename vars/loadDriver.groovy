e030a7f7a589678e0a45bad5cf16353338ab22e5
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 12:23:39 +0200

    Update loadDriver.groovy

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -1,10 +1,12 @@
 import java.sql.DriverManager;
 import groovy.sql.Sql
+import java.util.ServiceLoader;
+import java.sql.Driver;
 
-//@Grab(group='mysql', module='mysql-connector-java', version='8.0.21')
+@Grab(group='mysql', module='mysql-connector-java', version='8.0.21')
 class MyDB {
     def driver = Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); 
-    
+    ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
     def openConnection() {
         def conn = DriverManager.getConnection("jdbc:mysql://localhost/test?user=minty&password=greatsqldb");
     }
