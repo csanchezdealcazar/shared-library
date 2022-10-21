43a42f5c36c105eb3bba6aaf5cbd3f474a8862cc
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2022-10-21 10:08:46 +0200

    implement def call

--- vars/loadDriver.groovy
+++ vars/loadDriver.groovy
@@ -2,6 +2,7 @@
 @Grab("mysql:mysql-connector-java:8.0.31")
 
 import groovy.sql.Sql
-
-def sql = Sql.newInstance("jdbc:mysql://localhost:3306/test", "root", "root", "com.mysql.jdbc.Driver")
-def users = sql.rows("SELECT * FROM users")
+def call(Map config){
+ sql = Sql.newInstance("jdbc:mysql://localhost:3306/test", "root", "root", "com.mysql.jdbc.Driver")
+ users = sql.rows("SELECT * FROM users")
+}
